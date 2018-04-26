d65f454615d8c272e42368194b84295d48984252
ALEXIS DELUCIA <alexisdelucia23@gmail.com>
2018-04-26 00:18:26 -0400

    adding all corrections

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,147 +1,185 @@
 
-//below is the counter function
+var panel = $("#quiz-area");
 
+// Question set
+var questions = [{
+	question: "Halloween H20 features a cast member from Psycho. Who is it?",
+	answers: [" Anthony Perkins","Jamie Lee Curtis", "Norman Bates", "Janet Leigh"],
+	correctAnswer: "Janet Leigh"
+}, {
+	question: "What is Michael Myers middle name?",
+	answers: ["Audrey", "William","Anthony", "Thomas"],
+	correctAnswer: "Audrey"
+}, {
+	question: "In the Friday the 13th films, which state houses the Camp Crystal Lake?",
+	answers: ["New Mexico", "New Hampshire", "New England", "New Jersey"],
+	correctAnswer: "New Jersey"
+}, {
+	question: "Which of the following slashers has accumulated the highest body count?",
+	answers: ["Freddy Krueger", "Michael Myers", "Jason Voorhees", "Leatherface"],
+	correctAnswer: "Jason Voorhees"
+}, {
+	question: " Who was the killer in the original Friday the 13th movie?",
+	answers: ["Jason Voorhees", "Alice", "Mrs. Voorhees", "Jason's Dad"],
+	correctAnswer: "Mrs. Voorhees"
+}, {
+	question: "Which horror legend survives in Freddy vs. Jason ?",
+	answers: ["Freddy", "Jason", "Both", "Neither"],
+	correctAnswer: "Both"
+}, {
+	question: "In the Childs Play movies, what is the name of the boy that the killer repeatedly attempts to possess?",
+	answers: ["Andrew", "Andy", "Charle", "Robbie"],
+	correctAnswer: "Andy"
+}, {
+	question: "What year was the first Nightmare on Elm Street movie released?",
+	answers: ["1985", "1982", "1987", "1984"],
+	correctAnswer: "1984"
+}, {
+	question: "In Child's Play what is Chucky's real name?",
+	answers: ["Charles Lee Ray", "Charles Lee Roy", "Charles Chuckster Rayson", "Charles Leroy"],
+	correctAnswer: "Charles Lee Ray"
+}, {
+	question:" Who plays Freddy Krueger in the original Nightmare on Elms Street movies?",
+	answers: ["Kurl Russell", "Brian Yuzna", "Keith Davids", "Robert England"],
+	correctAnswer: "Robert England"
 
-var counter = 120;
-var timerCountdown = setInterval(function(){
-  
-  counter--
+}];
 
-  $("#timer").html("<h2>" + counter + " seconds remaining</h2>");
-  if (counter === 0) {
-    
-    clearInterval(timerCountdown);
-  }
-}, 1000);
+// Variable that will hold the setInterval
 
+var timer;
 
+// amount of correct and incorrect start at zero. Counter starts at 120 seconds
+var game = {
 
-$( document ).ready(function(){
+	correct: 0,
+	incorrect: 0,
+	counter: 120,
 
-//making var for the score tally
+	countdown: function() {
+    game.counter--;
+    $("#counter-number").html(game.counter);
+    if (game.counter === 0) {
+      console.log("TIME UP");
+      game.done();
+    }
+  },
+start: function() {
+    timer = setInterval(game.countdown, 1000);
 
-	var correct = 0;
-	var incorrect = 0;
+    $("#sub-wrapper").prepend("<h2>Time Remaining: <span id='counter-number'>120</span> Seconds</h2>");
 
-// below its stating on click of one of the multiple choice answers if the answer is the correct answer add 1 to the tally, if not add 1 to the incorrect tally. 
+    $("#start").remove();
 
-$("#mc").on ('click', function(){
-	var question1 = document.quiz.question1.value;
+    for (var i = 0; i < questions.length; i++) {
+      panel.append("<h2>" + questions[i].question + "</h2>");
+      for (var j = 0; j < questions[i].answers.length; j++) {
+        panel.append("<input type='radio' name='question-" + i +
+        "' value='" + questions[i].answers[j] + "''>" + questions[i].answers[j]);
+      }
+    }
 
-	if (question1 == "Janet Leigh") {
-		correct++;
+    panel.append("<button id='done'>Done</button>");
+  },
 
-		
-}
-	else  {
-		incorrect++
-	}
-});
+  done: function() {
 
-$("#mc").on ('click', function(){
-	var question2 = document.quiz.question2.value;
-
-	if (question2 == "Audrey") {
-		correct++;
-
-	
-}
-	else  {
-		incorrect++
-	}
-});
-
-$("#mc").on ('click', function(){
-	var question3 = document.quiz.question3.value;
-
-	if (question3 == "New Jersey") {
-		correct++;
-}
-	else  {
-		incorrect++
-	}
-});
-
-$("#mc").on ('click', function(){
-	var question4 = document.quiz.question4.value;
-
-	if (question4 == "Jason Voorhees") {
-		correct++;
-}
-	else  {
-		incorrect++
-	}
-});
-
-$("#mc").on ('click', function(){
-	var question5 = document.quiz.question5.value;
-
-	if (question5 == "Mrs. Voorhees") {
-		correct++;
-}
-	else  {
-		incorrect++
-	}
-});
-
-$("#mc").on ('click', function(){
-	var question6 = document.quiz.question6.value;
-
-	if (question6 == "Both") {
-		correct++;
-}
-	else  {
-		incorrect++
-	}
-});
-
-$("#mc").on ('click', function(){
-	var question7 = document.quiz.question7.value;
-
-	if (question7 == "Andy") {
-		correct++;
-}
-	else  {
-		incorrect++
-	}
-});	
-
-$("#mc").on ('click', function(){
-	var question8 = document.quiz.question8.value;
-
-	if (question8 == "1984") {
-		correct++;
-}
-	else  {
-		incorrect++
-	}
-});	
-
-$("#mc").on ('click', function(){
-	var question9 = document.quiz.question9.value;
-
-	if (question9 == "Charles Lee Ray") {
-		correct++;
-}
-	else  {
-		incorrect++
-	}
-});	
-
-$("#mc").on ('click', function(){
-	var question10 = document.quiz.question10.value;
-
-	if (question10 == "Robert Englund") {
-		correct++;
-}
-	else  {
-		incorrect++
-	}
-});
-
-$("sub").submit(function(){
-        // when form is clicked it submits and here should be code that displays on the page how many you got correct and incorrect
-
+    $.each($("input[name='question-0']:checked"), function() {
+      if ($(this).val() === questions[0].correctAnswer) {
+        game.correct++;
+      }
+      else {
+        game.incorrect++;
+      }
     });
 
+    $.each($("input[name='question-1']:checked"), function() {
+      if ($(this).val() === questions[1].correctAnswer) {
+        game.correct++;
+      }
+      else {
+        game.incorrect++;
+      }
+    });
+
+    $.each($("input[name='question-2']:checked"), function() {
+      if ($(this).val() === questions[2].correctAnswer) {
+        game.correct++;
+      }
+      else {
+        game.incorrect++;
+      }
+    });
+
+    $.each($("input[name='question-3']:checked"), function() {
+      if ($(this).val() === questions[3].correctAnswer) {
+        game.correct++;
+      }
+      else {
+        game.incorrect++;
+      }
+    });
+
+    $.each($("input[name='question-4']:checked"), function() {
+      if ($(this).val() === questions[4].correctAnswer) {
+        game.correct++;
+      }
+      else {
+        game.incorrect++;
+      }
+    });
+
+    $.each($("input[name='question-5']:checked"), function() {
+      if ($(this).val() === questions[5].correctAnswer) {
+        game.correct++;
+      }
+      else {
+        game.incorrect++;
+      }
+    });
+
+    $.each($("input[name='question-6']:checked"), function() {
+      if ($(this).val() === questions[6].correctAnswer) {
+        game.correct++;
+      }
+      else {
+        game.incorrect++;
+      }
+    });
+
+    $.each($("input[name='question-7']:checked"), function() {
+      if ($(this).val() === questions[7].correctAnswer) {
+        game.correct++;
+      }
+      else {
+        game.incorrect++;
+      }
+    });
+
+    this.result();
+
+  },
+
+  result: function() {
+
+    clearInterval(timer);
+
+    $("#sub-wrapper h2").remove();
+
+    panel.html("<h2>All Done!</h2>");
+    panel.append("<h3>Correct Answers: " + this.correct + "</h3>");
+    panel.append("<h3>Incorrect Answers: " + this.incorrect + "</h3>");
+    panel.append("<h3>Unanswered: " + (questions.length - (this.incorrect + this.correct)) + "</h3>");
+  }
+};
+
+// CLICK EVENTS
+
+$(document).on("click", "#start", function() {
+  game.start();
+});
+
+
+$(document).on("click", "#done", function() {
+  game.done();
 });
